1285328001feaebdfcd7cd9f787e081866bf53df
Beun de <tuanke.de.beun@pggm.nl>
2021-12-09 12:23:33 +0100

    My first commit

---
+++ InterestingFile.ps1
@@ -0,0 +1,6 @@
+Import-Module .\InterestingModule.psm1
+
+Write-Host "Hello World"
+$message = Get-Age
+Write-Host $message
+
---
+++ InterestingModule.psm1
@@ -0,0 +1,4 @@
+Function Get-Age {
+    $age = [int] (Read-Host "Enter your age! ")
+    return "Your are $age years old."
+}
