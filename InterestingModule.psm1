8e10669db1b934b3d7294bcbb6ff066484e07788
Tuanke de Beun <tuanke@live.nl>
2021-12-13 15:35:46 +0100

    name added in message

--- InterestingModule.psm1
+++ InterestingModule.psm1
@@ -1,4 +1,5 @@
 Function Get-Age {
+    $name = [string] (Read-Host "Enter your name! ")
     $age = [int] (Read-Host "Enter your age! ")
-    return "Your are $age years old."
+    return "Your name is $name and you are $age years old."
 }
