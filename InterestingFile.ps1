2f2ec817357307e2860e56d94b98314c6ee1d0c7
Tuanke de Beun <tuanke@live.nl>
2021-12-13 16:51:19 +0100

    Added header

--- InterestingFile.ps1
+++ InterestingFile.ps1
@@ -1,9 +1,14 @@
 Import-Module .\InterestingModule.psm1
 
 Write-Host "Hello World"
+
+# do function
 $message = Get-Age
 Write-Host $message
 
+# add header
+$message = "MESSAGE on {0} `{1}" -f (Get-Date |Out-String), $message
+# store message
 $message | Out-File ".\statement.txt"
 
 Write-Host "Program done"
