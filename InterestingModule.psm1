98e464bd008d1a89d72840f54e6b845cea6f1ca0
Tuanke de Beun <tuanke@live.nl>
2021-12-13 16:45:53 +0100

    implemented adulthood check

--- InterestingModule.psm1
+++ InterestingModule.psm1
@@ -1,5 +1,13 @@
 Function Get-Age {
     $name = [string] (Read-Host "Enter your name! ")
     $age = [int] (Read-Host "Enter your age! ")
+    
+    # assessment
+    if($age -ge 18) {
+        Write-Host "Pretty old, dude"
+    } else {
+        Write-Host "Get on, young lad!"
+    }
+
     return "Your name is $name and you are $age years old."
 }
